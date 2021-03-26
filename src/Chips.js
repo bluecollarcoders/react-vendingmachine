d85c06ddf492002165c22794c4f37a27df0f147d
Caleb Matteis <matteismusic@gmail.com>
2021-03-26 06:52:46 -0400

    commit

--- src/Chips.js
+++ src/Chips.js
@@ -5,6 +5,35 @@ import Message from "./Message";
 import "./Chips.css";
 
 function Chips() {
+    const [bags, setBags] = useState([]);
+
+    function handleClick() {
+        const x = window.innerWidth * Math.random();
+        const y = window.innerHeight * Math.random();
+        setBags(prevBags => [...prevBags, {x,y}]);
+    }
+
+    const b = bags.map((bag, i) => (
+        <img
+        key={i}
+        src={chipsImg}
+        className="bag"
+        style={{ top: `${bag.y}px`, left: `${bag.x}px` }}
+        alt="bag of lay's chips"
+        />
+    ));
+    return (
+        <div className="Chips">
+        <Message>
+            <h1>bags eaten: {bags.length}</h1>
+            <button onClick={handleClick}>oh soo good mmm</button>
+            <h1>
+                <Link to="/">go back</Link>
+            </h1>
+        </Message>
+           {b}
+        </div>
+    )
 
 }
 
