5f5ecf85b1f64d275cdf8684593f74955269d20e
Caleb Matteis <matteismusic@gmail.com>
2021-03-26 06:35:55 -0400

    commit

---
+++ src/Chips.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import chipsImg from "./Chips.png";
+import { Link } from "react-router-dom";
+import Message from "./Message";
+import "./Chips.css";
+
+function Chips() {
+
+}
+
+export default Chips;
---
+++ src/Message.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Message.css";
+
+
+function Message({children}) {
+    return (
+        <div className="Message">
+            {children}
+        </div>
+    )
+}
+
+export default Message;
